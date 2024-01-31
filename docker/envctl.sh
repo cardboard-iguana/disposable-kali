1d82ebd38b8d6a0dd6687b03765cfdc052e3c8ee
Nathan Acks <nathan.acks@cardboard-iguana.com>
2024-01-31 16:47:07 -0700

    Need to wait a little longer...

--- docker/envctl.sh
+++ docker/envctl.sh
@@ -216,7 +216,7 @@ removeContainerImagePair () {
 # Helper function that sleeps briefly.
 #
 waitForIt () {
-	SECONDS=8
+	SECONDS=10
 	echo -n ">>>> Sleeping briefly"
 	for STEP in $(seq 1 $SECONDS); do
 		sleep 1
