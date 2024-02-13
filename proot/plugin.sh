5e7e32aa09c649807de9ea262d673d17b72978c6
Nathan Acks <nathan.acks@cardboard-iguana.com>
2024-02-13 08:29:04 -0700

    Continued PRoot setup

--- proot/plugin.sh
+++ proot/plugin.sh
@@ -44,12 +44,140 @@ distro_setup() {
 			openssh-client \
 			tmux \
 			xclip \
-			xorg \
-			xorgxrdp \
-			xrdp
+			xorg
 
 		run_proot_cmd env DEBIAN_FRONTEND=noninteractive apt autoremove --quiet --assume-yes --purge --autoremove
 		run_proot_cmd env DEBIAN_FRONTEND=noninteractive apt clean      --quiet --assume-yes
+
+		# System configuration.
+		#
+		sed -i 's/^# en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/' $DISTRO_PREFIX/etc/locale.gen
+		run_proot_cmd locale-gen
+
+		run_proot_cmd ln -sf /usr/share/zoneinfo/$(getprop persist.sys.timezone) /etc/localtime
+
+		mv $DISTRO_PREFIX/usr/bin/systemctl $DISTRO_PREFIX/usr/bin/systemctl.bin
+
+		cat > $DISTRO_PREFIX/usr/bin/systemctl.sh <<- EOF
+		#!/usr/bin/env bash
+		service \$2 \$1
+		EOF
+
+		chmod 755 $DISTRO_PREFIX/usr/bin/systemctl.sh
+
+		cp $DISTRO_PREFIX/usr/bin/systemctl.sh $DISTRO_PREFIX/usr/bin/systemctl
+
+		run_proot_cmd env DEBIAN_FRONTEND=noninteractive make-ssl-cert generate-default-snakeoil
+
+		touch $DISTRO_PREFIX/root/.hushlogin
+
+		run_proot_cmd bash -c "msfdb init && /etc/init.d/postgresql stop"
+
+		# Make sure that problematic services are disabled (power
+		# management, screen saver, etc.).
+		#
+		rm --force $DISTRO_PREFIX/etc/xdg/autostart/nm-applet.desktop           &> /dev/null
+		rm --force $DISTRO_PREFIX/etc/xdg/autostart/xfce4-power-manager.desktop &> /dev/null
+		rm --force $DISTRO_PREFIX/etc/xdg/autostart/xscreensaver.desktop        &> /dev/null
+
+		sed -i 's/"cpugraph"/"cpugraph-disabled"/'                         $DISTRO_PREFIX/etc/xdg/xfce4/panel/default.xml
+		sed -i 's/"power-manager-plugin"/"power-manager-plugin-disabled"/' $DISTRO_PREFIX/etc/xdg/xfce4/panel/default.xml
+		sed -i 's/"pulseaudio"/"pulseaudio-disabled"/'                     $DISTRO_PREFIX/etc/xdg/xfce4/panel/default.xml
+		sed -i 's/"+lock-screen"/"-lock-screen"/'                          $DISTRO_PREFIX/etc/xdg/xfce4/panel/default.xml
+
+		# Create update script (useful for long-running containiners).
+		#
+		cat > $DISTRO_PREFIX/usr/local/bin/update.sh <<- EOF
+		#!/usr/bin/env bash
+
+		sudo cp /usr/bin/systemctl.bin /usr/bin/systemctl
+
+		sudo apt update
+		sudo apt full-upgrade
+		sudo apt autoremove --purge --autoremove
+		sudo apt clean
+
+		sudo cp /usr/bin/systemctl.sh /usr/bin/systemctl
+
+		sudo rm --force /etc/xdg/autostart/nm-applet.desktop           &> /dev/null
+		sudo rm --force /etc/xdg/autostart/xfce4-power-manager.desktop &> /dev/null
+		sudo rm --force /etc/xdg/autostart/xscreensaver.desktop        &> /dev/null
+
+		sudo sed -i 's/"cpugraph"/"cpugraph-disabled"/'                         /etc/xdg/xfce4/panel/default.xml
+		sudo sed -i 's/"power-manager-plugin"/"power-manager-plugin-disabled"/' /etc/xdg/xfce4/panel/default.xml
+		sudo sed -i 's/"pulseaudio"/"pulseaudio-disabled"/'                     /etc/xdg/xfce4/panel/default.xml
+		sudo sed -i 's/"+lock-screen"/"-lock-screen"/'                          /etc/xdg/xfce4/panel/default.xml
+
+		while IFS= read -d '' -r SKEL_DIR; do
+		    HOME_DIR="\$(echo "\$SKEL_DIR" | sed -e "s#^/etc/skel#\$HOME#")"
+		    mkdir -p "\$HOME_DIR"
+		done < <(find /etc/skel -mindepth 1 -type d -print0)
+
+		while IFS= read -d '' -r SKEL_FILE; do
+		    HOME_FILE="\$(echo "\$SKEL_FILE" | sed -e "s#^/etc/skel#\$HOME#")"
+		    cp -apf "\$SKEL_FILE" "\$HOME_FILE"
+		done < <(find /etc/skel -type f -print0)
+
+		ln -sf \$HOME/.face \$HOME/.face.icon
+
+		head --lines -1 /etc/skel/.java/.userPrefs/burp/prefs.xml > \$HOME/.java/.userPrefs/burp/prefs.xml
+		cat >> \$HOME/.java/.userPrefs/burp/prefs.xml << CONF
+		  <entry key="free.suite.alertsdisabledforjre-4166355790" value="true"/>
+		  <entry key="free.suite.alertsdisabledforjre-576990537" value="true"/>
+		  <entry key="free.suite.feedbackReportingEnabled" value="false"/>
+		  <entry key="eulacommunity" value="4"/>
+		</map>
+		CONF
+		EOF
+
+		chmod 755 $DISTRO_PREFIX/usr/local/bin/update.sh
+
+		# Create startup scripts.
+		#
+		cat > $DISTRO_PREFIX/usr/local/sbin/env-start.sh <<- EOF
+		#!/usr/bin/env bash
+
+		/etc/init.d/dbus start
+		/etc/init.d/postgresql start
+		EOF
+
+		cat > $DISTRO_PREFIX/usr/local/sbin/env-stop.sh <<- EOF
+		#!/usr/bin/env bash
+
+		/etc/init.d/postgresql stop
+		/etc/init.d/dbus stop
+		EOF
+
+		chmod 755 $DISTRO_PREFIX/usr/local/sbin/env-start.sh
+		chmod 755 $DISTRO_PREFIX/usr/local/sbin/env-stop.sh
+
+		cat > $DISTRO_PREFIX/usr/local/bin/tui.sh <<- EOF
+		#!/usr/bin/env bash
+
+		sudo /usr/local/sbin/env-start.sh
+
+		/usr/bin/env bash
+
+		sudo /usr/local/sbin/env-stop.sh
+		EOF
+
+		chmod 755 $DISTRO_PREFIX/usr/local/bin/tui.sh
+
+		cat > $DISTRO_PREFIX/usr/local/bin/gui.sh <<- EOF
+		#!/usr/bin/env bash
+
+		sudo /usr/local/sbin/env-start.sh
+
+		export DISPLAY=:0
+		export GALLIUM_DRIVER=virpipe
+		export MESA_GL_VERSION_OVERRIDE=4.0
+
+		dbus-launch --exit-with-session startxfce4
+
+		sudo /usr/local/sbin/env-stop.sh
+		EOF
+
+		chmod 755 $DISTRO_PREFIX/usr/local/bin/gui.sh
 	else
 		echo "Bad DISTRO_PREFIX! Post-install setup failed..."
 	fi
@@ -58,157 +186,6 @@ distro_setup() {
 #####################
 ## Dockerfile bits ##
 #####################
-
-# System configuration
-#
-RUN <<EOF
-sed -i 's/^# en_US.UTF-8 UTF-8$/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-EOF
-
-RUN --mount=type=secret,id=TIMEZONE \
-      ln -sf /usr/share/zoneinfo/$(cat /run/secrets/TIMEZONE) /etc/localtime
-
-RUN <<EOF
-mv /usr/bin/systemctl /usr/bin/systemctl.bin
-
-cat > /usr/bin/systemctl.sh <<SCRIPT
-#!/usr/bin/env bash
-service \$2 \$1
-SCRIPT
-
-chmod 755 /usr/bin/systemctl.sh
-
-cp /usr/bin/systemctl.sh /usr/bin/systemctl
-EOF
-
-RUN <<EOF
-export DEBIAN_FRONTEND=noninteractive
-
-make-ssl-cert generate-default-snakeoil
-
-touch /root/.hushlogin
-
-msfdb init
-/etc/init.d/postgresql stop
-
-unset DEBIAN_FRONTEND
-EOF
-
-# Make sure that problematic services are disabled (power management,
-# screen saver, etc.)
-#
-RUN <<EOF
-rm --force /etc/xdg/autostart/nm-applet.desktop           &> /dev/null
-rm --force /etc/xdg/autostart/xfce4-power-manager.desktop &> /dev/null
-rm --force /etc/xdg/autostart/xscreensaver.desktop        &> /dev/null
-
-sed -i 's/"cpugraph"/"cpugraph-disabled"/'                         /etc/xdg/xfce4/panel/default.xml
-sed -i 's/"power-manager-plugin"/"power-manager-plugin-disabled"/' /etc/xdg/xfce4/panel/default.xml
-sed -i 's/"pulseaudio"/"pulseaudio-disabled"/'                     /etc/xdg/xfce4/panel/default.xml
-sed -i 's/"+lock-screen"/"-lock-screen"/'                          /etc/xdg/xfce4/panel/default.xml
-EOF
-
-# Fix scroll on macOS
-#
-RUN --mount=type=secret,id=IS_MACOS <<EOF
-export IS_MACOS="$(cat /run/secrets/IS_MACOS)"
-
-if [ "$IS_MACOS" = "yes" ]; then
-    sed -i 's/^\[SessionVariables\]$/\[SessionVariables\]\nXRDP_XORG_TOUCHPAD_SCROLL_HACK=yes/' /etc/xrdp/sesman.ini
-fi
-
-unset IS_MACOS
-EOF
-
-# Create update script (useful for long-running containiners)
-#
-RUN --mount=type=secret,id=IS_MACOS <<EOF
-export IS_MACOS="$(cat /run/secrets/IS_MACOS)"
-
-cat > /usr/local/bin/update.sh << SCRIPT
-#!/usr/bin/env bash
-
-sudo cp /usr/bin/systemctl.bin /usr/bin/systemctl
-
-sudo apt update
-sudo apt full-upgrade
-sudo apt autoremove --purge --autoremove
-sudo apt clean
-
-sudo cp /usr/bin/systemctl.sh /usr/bin/systemctl
-
-sudo rm --force /etc/xdg/autostart/nm-applet.desktop           &> /dev/null
-sudo rm --force /etc/xdg/autostart/xfce4-power-manager.desktop &> /dev/null
-sudo rm --force /etc/xdg/autostart/xscreensaver.desktop        &> /dev/null
-
-sudo sed -i 's/"cpugraph"/"cpugraph-disabled"/'                         /etc/xdg/xfce4/panel/default.xml
-sudo sed -i 's/"power-manager-plugin"/"power-manager-plugin-disabled"/' /etc/xdg/xfce4/panel/default.xml
-sudo sed -i 's/"pulseaudio"/"pulseaudio-disabled"/'                     /etc/xdg/xfce4/panel/default.xml
-sudo sed -i 's/"+lock-screen"/"-lock-screen"/'                          /etc/xdg/xfce4/panel/default.xml
-
-SCRIPT
-
-if [ "$IS_MACOS" = "yes" ]; then
-cat >> /usr/local/bin/update.sh << SCRIPT
-if [[ \$(grep -Ec '^XRDP_XORG_TOUCHPAD_SCROLL_HACK=yes$' /etc/xrdp/sesman.ini) -eq 0 ]]; then
-    sudo sed -i 's/^\[SessionVariables\]$/\[SessionVariables\]\nXRDP_XORG_TOUCHPAD_SCROLL_HACK=yes/' /etc/xrdp/sesman.ini
-fi
-
-SCRIPT
-fi
-
-cat >> /usr/local/bin/update.sh << SCRIPT
-while IFS= read -d '' -r SKEL_DIR; do
-    HOME_DIR="\$(echo "\$SKEL_DIR" | sed -e "s#^/etc/skel#\$HOME#")"
-    mkdir -p "\$HOME_DIR"
-done < <(find /etc/skel -mindepth 1 -type d -print0)
-
-while IFS= read -d '' -r SKEL_FILE; do
-    HOME_FILE="\$(echo "\$SKEL_FILE" | sed -e "s#^/etc/skel#\$HOME#")"
-    cp -apf "\$SKEL_FILE" "\$HOME_FILE"
-done < <(find /etc/skel -type f -print0)
-
-ln -sf \$HOME/.face \$HOME/.face.icon
-
-head --lines -1 /etc/skel/.java/.userPrefs/burp/prefs.xml > \$HOME/.java/.userPrefs/burp/prefs.xml
-cat >> \$HOME/.java/.userPrefs/burp/prefs.xml << CONF
-  <entry key="free.suite.alertsdisabledforjre-4166355790" value="true"/>
-  <entry key="free.suite.alertsdisabledforjre-576990537" value="true"/>
-  <entry key="free.suite.feedbackReportingEnabled" value="false"/>
-  <entry key="eulacommunity" value="4"/>
-</map>
-CONF
-
-find -L /usr/share/nano -type f -iname '*.nanorc' | sed 's/^/include /' > \$HOME/.nanorc
-SCRIPT
-
-chmod 755 /usr/local/bin/update.sh
-
-unset IS_MACOS
-EOF
-
-# Create Docker startup script
-#
-RUN --mount=type=secret,id=USER_NAME <<EOF
-export USER_NAME="$(cat /run/secrets/USER_NAME)"
-
-cat > /usr/local/sbin/docker-init.sh << SCRIPT
-#!/usr/bin/env bash
-
-/etc/init.d/dbus start
-/etc/init.d/postgresql start
-
-rm --recursive --force /var/run/xrdp &> /dev/null
-/etc/init.d/xrdp start
-
-chown --recursive $USER_NAME:$USER_NAME /home/$USER_NAME
-
-/usr/bin/bash
-SCRIPT
-
-chmod 755 /usr/local/sbin/docker-init.sh
-EOF
 
 # Create our normal user
 #
@@ -359,6 +336,7 @@ CONF
 cat > $USER_HOME/.tmux.conf << CONF
 set -g default-terminal "tmux-256color"
 set -g allow-passthrough off
+set -g history-limit 16383
 
 set -g mouse on
 set-option -s set-clipboard off
@@ -366,7 +344,7 @@ bind-key -T copy-mode    MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xc
 bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip -selection clipboard"
 CONF
 
-find -L /usr/share/nano -type f -iname '*.nanorc' | sed 's/^/include /' > $USER_HOME/.nanorc
+echo "set tabsize 4" > $USER_HOME/.nanorc
 
 mkdir --parents $USER_HOME/Documents
 
