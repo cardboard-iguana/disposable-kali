2dc4c72ee2d90667299fd8455528d77995acba5b
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-04-27 09:18:10 -0600

    Drop Docker/Podman support (for now?)

--- README.md
+++ README.md
@@ -1,60 +1,9 @@
 # Displosable Kali Linux
-Disposable [Kali Linux](https://kali.org) environments using [Podman](https://podman.io/) (macOS & Linux) or [PRoot Distro](https://github.com/termux/proot-distro) (Android).
+Disposable [Kali Linux](https://kali.org) environments for Android using [PRoot Distro](https://github.com/termux/proot-distro).
 
 This setup is probably sufficiently opinionated that it won't be useful out-of-the box for `$RANDOM_HACKER`. However, it is provided here as a resource for others to study, fork, and draw inspiration from. (Also, I'm not adverse to integrating other folks' suggestions! I'm just not going to make any changes that either make my own use cases more complicated or this repo more difficult to maintain in general. Within those bounds though, issues and pull requests are welcome!)
 
 ## Prerequisits
-### macOS
-- [Podman](https://podman.io/)
-- [Microsoft Remote Desktop](https://apps.apple.com/us/app/microsoft-remote-desktop/id1295203466) (or another RDP client)
-
-The use of [Homebrew](https://brew.sh) to install these tools is *highly* recommended. You can probably make things work without it, but you'll need to make sure your PATH is correct at the *GUI* level!
-
-```bash
-# Make sure that command-line developer tools are available
-#
-xcode-select --install
-
-# Install Homebrew (`brew` path will be /usr/local/bin on x86_64 systems)
-#
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> $HOME/.zshrc
-eval "$(/opt/homebrew/bin/brew shellenv)"
-brew analytics off
-
-# Install actual prerequisits
-#
-brew install microsoft-remote-desktop podman
-```
-
-**Note:** Podman is weird about how the default virtual machine gets set. If you use multiple virtual machines (*i.e.*, `podman machine list` has more entries than just `podman-machine-default`), you'll probably periodically get errors where the `$CONTROL_SCRIPT` can't find your container. If you see this, try resetting the default VM using ``podman system connection default podman-machine-default`.
-
-Finally, make sure that `$HOME/.local/bin` exists and is in your `$PATH`.
-
-### Linux
-- [bc](https://www.gnu.org/software/bc/)
-- [Podman](https://podman.io/)
-- `uuidgen`
-- [XFreeRDP or wlFreeRDP](https://www.freerdp.com/)
-
-On [Debian](https://debian.org/):
-
-```bash
-# Alternately, replace `freerdp2-x11` with `freerdp2-wayland`.
-#
-apt install bc freerdp2-x11 podman uuid-runtime
-```
-
-**Note:** You may need to add sub-UIDs/GIDs for your user:
-
-```bash
-[[ $(grep -c $USER /etc/subuid) -eq 0 ]] && sudo usermod --add-subuid 100000-165535 $USER
-[[ $(grep -c $USER /etc/subgid) -eq 0 ]] && sudo usermod --add-subgid 100000-165535 $USER
-```
-
-Finally, make sure that `$HOME/.local/bin` exists and is in your `$PATH`.
-
-### Android
 - [Termux](https://f-droid.org/en/packages/com.termux/)
 - [Termux:X11](https://github.com/termux/termux-x11)
 
@@ -160,23 +109,8 @@ You will almost certainly want to enable Developer Mode and then set **Developer
 ## Usage
 To create a new engagement (container, control script, and data directory), just clone this repo and then run `bash mkenv.sh some-engagement-name` from inside of it.
 
-At the end of the process, the control script name will be provided and the script's "help" command will automatically run. Script commands are detailed below.
+At the end of the process, the control script name will be provided and the script's "help" command will automatically run.
 
-### Linux and macOS
-- `$CONTROL_SCRIPT --help`: Display help message.
-- `$CONTROL_SCRIPT --start`: Start the engagement's container.
-- `$CONTROL_SCRIPT --stop`: Stop the engagement's container.
-- `$CONTROL_SCRIPT --desktop`: Connect to a desktop in the engagement's container. Automatically calls `$CONTROL_SCRIPT --start` if necessary.
-- `$CONTROL_SCRIPT --update`: Update the engagement container's packages. Useful during long-running engagements.
-- `$CONTROL_SCRIPT --backup`: Backup the engagement container in the data directory. Useful for taking snapshots of the container before making a potentially destructive change during an engagement or porting a configured engagement to a different machine.
-- `$CONTROL_SCRIPT --restore`: Removes the current engagement container and image and regenerates it from the backup linked at `$ENGAGEMENT_DIR/Backups/${NAME}.tar`. By default this will be the most recent backup, but the symlink can be changed manually to point to any other backup.
-- `$CONTROL_SCRIPT --archive`: Backup the engagement container, delete it and the associated image, and archive the control script in the data directory. This is generally what you'll want to do at the end of the engagement.
-
-**Note:** On macOS, the first time you start an environment's desktop, you will be asked to grant either the Terminal app or `env` "Accessibility Access". You will need to do this in the System Settings app and then re-connect. On the next run, you will be asked to grant access to the "System Events" app. You should not be asked for these permissions again, or for subsequent engagements.
-
-If `$CONTROL_SCRIPT` is called without any options, then a tmux shell is opened in the engagement environment (`$CONTROL_SCRIPT --start` is called first, if necessary).
-
-### Android
 - `$CONTROL_SCRIPT --help`: Display help message.
 - `$CONTROL_SCRIPT --update`: Update the engagement environment's packages. Useful during long-running engagements.
 - `$CONTROL_SCRIPT --backup`: Backup the engagement environment in the data directory. Useful for taking snapshots of the container before making a potentially destructive change during an engagement or porting a configured engagement to a different machine.
--- mkenv.sh
+++ mkenv.sh
@@ -2,73 +2,9 @@
 
 set -e
 
-OS="$(uname)"
-
-# Figure out which podman we're using; we need at least v4.9.3 to get
-# reliable builds. Once this is widely available (Debian 13 trixie for
-# Chrome OS, but maybe worth keeping this check in here until I get a
-# good look at SteamOS), we can just revert back to using the system
-# podman everywhere.
-#
-if [[ "$OS" == "Darwin" ]]; then
-	PODMAN="$(which podman)"
-elif [[ -z "$PREFIX" ]]; then
-	mkdir -p "$HOME/.cache/disposable-kali"
-
-	LATEST_PODMAN_LAUNCHER="$(curl --silent --location https://api.github.com/repos/89luca89/podman-launcher/releases/latest | grep --perl-regexp --only-matching '"tag_name": ?"v\K.*?(?=")')"
-
-	if [[ -f "$HOME/.cache/disposable-kali/podman-launcher.version" ]]; then
-		CURRENT_PODMAN_LAUNCHER="$(cat "$HOME/.cache/disposable-kali/podman-launcher.version")"
-	else
-		CURRENT_PODMAN_LAUNCHER="0.0.0"
-	fi
-
-	if [[ "$LATEST_PODMAN_LAUNCHER" != "$CURRENT_PODMAN_LAUNCHER" ]]; then
-		if [[ "$(uname -m)" == "aarch64" ]]; then
-			PODMAN_LAUNCHER_ARCH="arm64"
-		else
-			PODMAN_LAUNCHER_ARCH="amd64"
-		fi
-
-		curl --location --output "$HOME/.cache/disposable-kali/podman-launcher" "https://github.com/89luca89/podman-launcher/releases/download/v${LATEST_PODMAN_LAUNCHER}/podman-launcher-${PODMAN_LAUNCHER_ARCH}"
-
-		chmod +x "$HOME/.cache/disposable-kali/podman-launcher"
-		echo "$LATEST_PODMAN_LAUNCHER" > "$HOME/.cache/disposable-kali/podman-launcher.version"
-	fi
-
-	PODMAN_LAUNCHER_PODMAN_VERSION="$("$HOME/.cache/disposable-kali/podman-launcher" version | grep '^Version: .*' | sed 's/.* //')"
-	echo "$PODMAN_LAUNCHER_PODMAN_VERSION" > "$HOME/.cache/disposable-kali/podman-launcher.podman.version"
-
-	if [[ -n "$(which podman 2> /dev/null)" ]]; then
-		SYSTEM_PODMAN_VERSION="$(podman version | grep '^Version: .*' | sed 's/.* //')"
-	else
-		SYSTEM_PODMAN_VERSION="0.0.0"
-	fi
-
-	MOST_RECENT_VERSION="$(echo -e "${SYSTEM_PODMAN_VERSION}\n${PODMAN_LAUNCHER_PODMAN_VERSION}" | sort --version-sort --reverse | head -1)"
-
-	if [[ "$MOST_RECENT_VERSION" == "$SYSTEM_PODMAN_VERSION" ]]; then
-		PODMAN="$(which podman)"
-	else
-		PODMAN="$HOME/.cache/disposable-kali/podman-launcher"
-	fi
-else
-	PODMAN=""
-fi
-
 # Sanity check.
 #
-if [[ -z "$PREFIX" ]] && [[ -n "$PODMAN" ]]; then
-	if [[ ! -f container/envctl.sh ]] || [[ ! -f container/Dockerfile ]]; then
-		echo "This script must be run from the root of the disposable-kali repo!"
-		exit 1
-	fi
-	if [[ ":$PATH:" != *":$HOME/.local/bin:"* ]]; then
-		echo "$HOME/.local/bin must bin in your PATH!"
-		exit 1
-	fi
-	CODE_PATH="podman"
-elif [[ -n "$PREFIX" ]] && [[ -n "$(which proot-distro)" ]]; then
+if [[ -n "$PREFIX" ]] && [[ -n "$(which proot-distro)" ]]; then
 	if [[ ! -f proot/envctl.sh ]] || [[ ! -f proot/plugin.sh ]]; then
 		echo "This script must be run from the root of the disposable-kali repo!"
 		exit 1
@@ -79,7 +15,7 @@ elif [[ -n "$PREFIX" ]] && [[ -n "$(which proot-distro)" ]]; then
 	fi
 	CODE_PATH="proot"
 else
-	echo "No usable install of Podman or PRoot Distro found!"
+	echo "No usable install of PRoot Distro found!"
 	exit 1
 fi
 
@@ -94,217 +30,60 @@ fi
 
 # Determine build variables.
 #
-if [[ "$CODE_PATH" == "podman" ]]; then
-	SCRIPT="$HOME/.local/bin/${NAME}"
-	ENGAGEMENT_DIR="$HOME/Engagements/$NAME"
-elif [[ "$CODE_PATH" == "proot" ]]; then
-	SCRIPT="$HOME/bin/${NAME}"
-	ENGAGEMENT_DIR="$HOME/storage/shared/Documents/Engagements/$NAME"
-else
-	echo "You should not be here."
-	exit 2
-fi
+SCRIPT="$HOME/bin/${NAME}"
+ENGAGEMENT_DIR="$HOME/storage/shared/Documents/Engagements/$NAME"
 
 # Create necessary directories.
 #
 mkdir -p "$ENGAGEMENT_DIR"
 mkdir -p "$(dirname "$SCRIPT")"
 
-# Build container/proot.
+# PRoot Distro engages in some serious nannying around pentesting
+# distros. While I understand the Termux project's desire not to
+# support script-kiddies, and support their refusal to include
+# hacking tools (even if it makes my life harder), actively
+# subverting user requests is, in my opinion, a step too far.
 #
-if [[ "$CODE_PATH" == "podman" ]]; then
-	# Init the Podman VM, if necessary.
-	#
-	if [[ "$OS" == "Darwin" ]]; then
-		PODMAN_MACHINE_NAME="$("$PODMAN" machine list --format "{{.Default}}\t{{.Name}}" 2> /dev/null | grep -E '^true' | cut -f 2 | sed 's/\*$//')"
+#     https://github.com/termux/proot-distro/commit/470525c55020d72b66b509066b8d71d59b62072c
+#
+# Proactively un-nerf pentest capabilities (even though we probably
+# won't need that functionality ourselves in most cases).
+#
+sed -i 's/if .*(kali|parrot|nethunter|blackarch).*; then/if false; then/' $(which proot-distro)
 
-		if [[ -z "$PODMAN_MACHINE_NAME" ]]; then
-			TOTAL_AVAIL_MEMORY=$(bc -le "mem = (($(sysctl hw.memsize | sed 's/.*: //') / 1024) / 1024) / 2; scale = 0; mem / 1")
-			if [[ $TOTAL_AVAIL_MEMORY -ge 32768 ]]; then
-				VM_MEMORY=32768
-			elif [[ $TOTAL_AVAIL_MEMORY -ge 16384 ]]; then
-				VM_MEMORY=16384
-			elif [[ $TOTAL_AVAIL_MEMORY -ge 8192 ]]; then
-				VM_MEMORY=8192
-			elif [[ $TOTAL_AVAIL_MEMORY -ge 4096 ]]; then
-				VM_MEMORY=4096
-			else
-				echo "At least 8 GB of memory is required!"
-				exit 1
-			fi
+TARBALL_SHA256="$(curl https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-minimal-arm64.tar.xz | sha256sum | sed 's/ .*//')"
+BUILD_DATE="$(date)"
 
-			TOTAL_AVAIL_CPU=$(bc -le "cpu = $(sysctl hw.ncpu | sed 's/.*: //') / 2; scale = 0; cpu / 1")
-			if [[ $TOTAL_AVAIL_CPU -ge 16 ]]; then
-				VM_CPU=16
-			elif [[ $TOTAL_AVAIL_CPU -ge 8 ]]; then
-				VM_CPU=8
-			elif [[ $TOTAL_AVAIL_CPU -ge 4 ]]; then
-				VM_CPU=4
-			elif [[ $TOTAL_AVAIL_CPU -ge 2 ]]; then
-				VM_CPU=2
-			else
-				echo "At least 4 CPU cores are required!"
-				exit 1
-			fi
+sed "s|{{distro-name}}|$NAME|;s|{{build-date}}|$BUILD_DATE|;s|{{tarball-sha256}}|$TARBALL_SHA256|" proot/plugin.sh > "$PREFIX/etc/proot-distro/${NAME}.override.sh"
 
-			TOTAL_AVAIL_DISK=$(bc -le "disk = (($(df -Pk $HOME | tail -1 | sed 's/[ ]\{1,\}/\t/g' | cut -f 4) / 1024) / 1024) * 0.64; scale = 0; disk / 1")
-			if [[ $TOTAL_AVAIL_DISK -ge 512 ]]; then
-				VM_DISK=512
-			elif [[ $TOTAL_AVAIL_DISK -ge 256 ]]; then
-				VM_DISK=256
-			elif [[ $TOTAL_AVAIL_DISK -ge 128 ]]; then
-				VM_DISK=128
-			else
-				echo "At least 200 GB of free disk space is required!"
-				exit 1
-			fi
+proot-distro install "$NAME"
+proot-distro clear-cache
 
-			"$PODMAN" machine init --cpus=$VM_CPU --disk-size=$VM_DISK --memory=$VM_MEMORY
-			"$PODMAN" machine start --no-info
+# For some reason, setting up PostgreSQL/Metasploit works when
+# called from proot-distro, but does not work when called using the
+# run_proot_cmd helper function when building a new environment.
+# For anyone interested in trying to track this down, the error
+# message generated by pg_createcluster is:
+#
+#     FATAL:   Could not create shared memory segment: Function not implemented
+#     DETAIL:  Failed system call was shmget
+#
+proot-distro login "$NAME" --no-arch-warning -- bash -c "su postgres --command=\"pg_createcluster 17 main\" && su postgres --command=\"/etc/init.d/postgresql start\" && msfdb init && su postgres --command=\"/etc/init.d/postgresql stop\""
 
-			# Fix https://github.com/containers/podman/issues/22678.
-			#
-			if [[ $("$PODMAN" machine ssh 'sudo rpm-ostree status' | grep -c 'podman-machine-os:5.0') -gt 0 ]]; then
-				"$PODMAN" machine os apply quay.io/podman/machine-os:5.1 --restart
-			fi
+# Set user password.
+#
+proot-distro login "$NAME" --no-arch-warning -- bash -c "echo \"kali:\$(uuidgen --random)\" | chpasswd"
 
-			# Make sure that auto-updates are disabled
-			#
-			"$PODMAN" machine ssh 'sudo systemctl disable --now zincati.service'
+# Create dedicated desktop launchers.
+#
+mkdir --parents "$HOME/.local/share/applications"
+cat "$PREFIX/var/lib/proot-distro/installed-rootfs/$NAME/usr/share/applications/code-oss.desktop" | sed "s#^Name=#Name=[$NAME] #;s#^Exec=#Exec=$SCRIPT #;s#^Categories=.*#Categories=Security#" > "$HOME/.local/share/applications/${NAME}-code-oss.desktop"
+cat "$PREFIX/var/lib/proot-distro/installed-rootfs/$NAME/usr/share/applications/firefox-esr.desktop" | sed "s#^Name=#Name=[$NAME] #;s#^Exec=#Exec=$SCRIPT #;s#^Categories=.*#Categories=Security#" > "$HOME/.local/share/applications/${NAME}-firefox-esr.desktop"
+cat "$PREFIX/var/lib/proot-distro/installed-rootfs/$NAME/usr/share/applications/kali-burpsuite.desktop" | sed "s#^Name=#Name=[$NAME] #;s#^Exec=#Exec=$SCRIPT #;s#^Categories=.*#Categories=Security#" > "$HOME/.local/share/applications/${NAME}-burp-suite.desktop"
 
-			# Perform an update (updates roll out every 14 days, so maybe once per week?)
-			#
-			"$PODMAN" machine ssh 'sudo rpm-ostree upgrade'
-			"$PODMAN" machine stop
-			"$PODMAN" machine start --no-info
-
-			mkdir -p $HOME/.cache/containerized-engagements
-			date "+%s" > $HOME/.cache/containerized-engagements/machine-update
-		else
-			if [[ "$("$PODMAN" machine inspect --format "{{.State}}" "$PODMAN_MACHINE_NAME" 2> /dev/null)" != "running" ]]; then
-				"$PODMAN" machine start --no-info
-			fi
-		fi
-	fi
-
-	# Build container.
-	#
-	# Note the use of --no-cache for `podman build`. The reason here is
-	# that changing the username, timezone, and password changes doesn't
-	# necessarily invalidate the build cache, since secrets aren't
-	# exposed (generally a good thing). While we could try to cache bust
-	# out way out of the situation, we still want to grab the latest
-	# packages, so most of our build will be invalid anyway.
-	#
-	# Building without a cache isn't quite as bad as it sounds, because
-	# in practice most of the time a new engagement is only going to be
-	# built daily at worse, and more likely only once every 2 - 3 weeks.
-	#
-	HOST_SPECIFIC_FLAGS=()
-	if [[ "$OS" == "Darwin" ]]; then
-		CONNECTION_TOKEN="$(uuidgen | tr "[:upper:]" "[:lower:]")"
-	else
-		CONNECTION_TOKEN="$(uuidgen --random)"
-		HOST_SPECIFIC_FLAGS+=(--userns keep-id:uid=1000,gid=1000)
-	fi
-
-	TIMEZONE="$(readlink /etc/localtime | sed 's#.*/zoneinfo/##')"
-
-	TOKEN_FILE="$(mktemp)"
-
-	echo "$CONNECTION_TOKEN" > "$TOKEN_FILE"
-
-	cat container/Dockerfile | "$PODMAN" build \
-		--no-cache \
-		--build-arg HOST_OS="$OS" \
-		--build-arg TIMEZONE="$TIMEZONE" \
-		--build-arg USER_NAME="$USER" \
-		--secret id=connection-token,src="$TOKEN_FILE" \
-		--squash-all \
-		--tag "$NAME" -
-
-	rm "$TOKEN_FILE"
-
-	mkdir -p $HOME/.cache/disposable-kali
-	echo "$TIMEZONE" > $HOME/.cache/disposable-kali/localtime
-
-	"$PODMAN" create --name "$NAME" \
-	                 --publish 127.0.0.1:3389:3389 \
-	                 --tty \
-	                 --mount type=bind,source="$ENGAGEMENT_DIR",destination=/home/$USER/Documents \
-	                 --mount type=bind,source=$HOME/.cache/disposable-kali/localtime,destination=/etc/localtime.host,readonly \
-	                   "${HOST_SPECIFIC_FLAGS[@]}" "$NAME"
-
-	"$PODMAN" builder prune --all --force > /dev/null
-	"$PODMAN" image   prune --all --force > /dev/null
-
-	# Shut down the Podman VM, unless it's still being used for something.
-	#
-	if [[ "$OS" == "Darwin" ]]; then
-		if [[ $("$PODMAN" container list --format "{{.State}}" | grep -c "running") -eq 0 ]]; then
-			"$PODMAN" machine stop
-		fi
-	fi
-
-	# Setup control script.
-	#
-	sed "s/{{environment-name}}/$NAME/;s/{{connection-token}}/$CONNECTION_TOKEN/" container/envctl.sh > "$SCRIPT"
-
-elif [[ "$CODE_PATH" == "proot" ]]; then
-	# PRoot Distro engages in some serious nannying around pentesting
-	# distros. While I understand the Termux project's desire not to
-	# support script-kiddies, and support their refusal to include
-	# hacking tools (even if it makes my life harder), actively
-	# subverting user requests is, in my opinion, a step too far.
-	#
-	#     https://github.com/termux/proot-distro/commit/470525c55020d72b66b509066b8d71d59b62072c
-	#
-	# Proactively un-nerf pentest capabilities (even though we probably
-	# won't need that functionality ourselves in most cases).
-	#
-	sed -i 's/if .*(kali|parrot|nethunter|blackarch).*; then/if false; then/' $(which proot-distro)
-
-	TARBALL_SHA256="$(curl https://kali.download/nethunter-images/current/rootfs/kali-nethunter-rootfs-minimal-arm64.tar.xz | sha256sum | sed 's/ .*//')"
-	BUILD_DATE="$(date)"
-
-	sed "s|{{distro-name}}|$NAME|;s|{{build-date}}|$BUILD_DATE|;s|{{tarball-sha256}}|$TARBALL_SHA256|" proot/plugin.sh > "$PREFIX/etc/proot-distro/${NAME}.override.sh"
-
-	proot-distro install "$NAME"
-	proot-distro clear-cache
-
-	# For some reason, setting up PostgreSQL/Metasploit works when
-	# called from proot-distro, but does not work when called using the
-	# run_proot_cmd helper function when building a new environment.
-	# For anyone interested in trying to track this down, the error
-	# message generated by pg_createcluster is:
-	#
-	#     FATAL:   Could not create shared memory segment: Function not implemented
-	#     DETAIL:  Failed system call was shmget
-	#
-	proot-distro login "$NAME" --no-arch-warning -- bash -c "su postgres --command=\"pg_createcluster 17 main\" && su postgres --command=\"/etc/init.d/postgresql start\" && msfdb init && su postgres --command=\"/etc/init.d/postgresql stop\""
-
-	# Set user password.
-	#
-	proot-distro login "$NAME" --no-arch-warning -- bash -c "echo \"kali:\$(uuidgen --random)\" | chpasswd"
-
-	# Create dedicated desktop launchers.
-	#
-	mkdir --parents "$HOME/.local/share/applications"
-	cat "$PREFIX/var/lib/proot-distro/installed-rootfs/$NAME/usr/share/applications/code-oss.desktop" | \
-		sed "s#^Name=#Name=[$NAME] #;s#^Exec=#Exec=$SCRIPT #;s#^Categories=.*#Categories=Security#" > "$HOME/.local/share/applications/${NAME}-code-oss.desktop"
-	cat "$PREFIX/var/lib/proot-distro/installed-rootfs/$NAME/usr/share/applications/firefox-esr.desktop" | \
-		sed "s#^Name=#Name=[$NAME] #;s#^Exec=#Exec=$SCRIPT #;s#^Categories=.*#Categories=Security#" > "$HOME/.local/share/applications/${NAME}-firefox-esr.desktop"
-	cat "$PREFIX/var/lib/proot-distro/installed-rootfs/$NAME/usr/share/applications/kali-burpsuite.desktop" | \
-		sed "s#^Name=#Name=[$NAME] #;s#^Exec=#Exec=$SCRIPT #;s#^Categories=.*#Categories=Security#" > "$HOME/.local/share/applications/${NAME}-burp-suite.desktop"
-
-	# Setup control script.
-	#
-	sed "s/{{environment-name}}/$NAME/" proot/envctl.sh > "$SCRIPT"
-
-else
-	echo "You should not be here."
-	exit 2
-fi
+# Setup control script.
+#
+sed "s/{{environment-name}}/$NAME/" proot/envctl.sh > "$SCRIPT"
 
 # Finish up.
 #
@@ -315,12 +94,5 @@ echo "$SCRIPT."
 echo ""
 echo "------------------------------------------------------------------------"
 echo ""
-if [[ "$CODE_PATH" == "podman" ]]; then
-	"$SCRIPT" help
-elif [[ "$CODE_PATH" == "proot" ]]; then
-	"$SCRIPT" --help
-else
-	echo "You should not be here."
-	exit 2
-fi
+"$SCRIPT" --help
 echo ""
